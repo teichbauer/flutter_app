de770b3ea9d725fab19f181f7e7a646c5619864c
raymond wei <raymondwei@outlook.com>
2019-12-27 21:12:08 -0600

    0.0.3-20191227 - #6 use downloaded font from fonts.google.com -> IndieFlower

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first app'),
       centerTitle: true,
+      backgroundColor: Colors.red[500],
     ),
     body: Center(
-      child: Text("hello Ninjas"),
+      child: Text(
+          "hello Ninjas",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
-      child: Text('child'),
+      onPressed: (){},
+      child: Text('click!'),
+      backgroundColor: Colors.red[400],
     ),
   ),
 ));
