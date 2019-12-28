38ebfe67902a244b15255d6822d94760c60add1f
raymond wei <raymondwei@outlook.com>
2019-12-28 13:25:58 -0600

    0.0.4-20191228 - #7: hot reload, with a custom widget inheriting from StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[500],
-    ),
-    body: Center(
-      child: Text(
-          "hello Ninjas",
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[500],
+      ),
+      body: Center(
+        child: Text(
+          "Hello Ninjias", //"大虾们好",
           style: TextStyle(
-            fontSize: 20.0,
+            fontSize: 30.0,
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
             color: Colors.grey[600],
             fontFamily: 'IndieFlower',
           ),
+        ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('click!'),
-      backgroundColor: Colors.red[400],
-    ),
-  ),
-));
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('click that'),
+        backgroundColor: Colors.red[400],
+      ),
+    );
+  }
+}
