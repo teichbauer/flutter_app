d8a4065b62c9c41834db79465969ff3fd7614f59
raymond wei <raymondwei@outlook.com>
2020-06-25 00:31:07 -0500

    0.0.5-20200624 - playing bit more. - github remote

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: Home(), // root widget
     ));
 
 class Home extends StatelessWidget {
@@ -9,13 +9,14 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
+        title: Text('工业 4.0'),
         centerTitle: true,
-        backgroundColor: Colors.red[500],
+        //backgroundColor: Colors.blue[500],
+        backgroundColor: Colors.blueGrey,
       ),
       body: Center(
         child: Text(
-          "Hello Ninjas", //"大虾们好",
+          "大虾们好", //"大虾们好",
           style: TextStyle(
             fontSize: 30.0,
             fontWeight: FontWeight.bold,
@@ -27,8 +28,8 @@ class Home extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click that'),
-        backgroundColor: Colors.red[400],
+        child: Text('点击'),
+        backgroundColor: Colors.blueGrey,
       ),
     );
   }
